497579e8d4b1ff960f9183dfb492870e95bccba7
spencerdearman <121642493+spencerdearman@users.noreply.github.com>
2024-01-16 10:17:38 -0600

    Add files via upload

---
+++ RealityApp/RealityApp/RealityAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RealityAppApp.swift
+//  RealityApp
+//
+//  Created by Spencer Dearman on 1/16/24.
+//
+
+import SwiftUI
+
+@main
+struct RealityAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
